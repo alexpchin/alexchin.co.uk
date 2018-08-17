84653fd6c610ec47c420d657bd393807ce998963
alexpchin <alex@alexchin.co.uk>
2018-08-17 21:03:56 +0100

    Adding a static.json file to override the buildpack

--- src/index.js
+++ src/index.js
@@ -14,4 +14,5 @@ if (module.hot) {
   });
 }
 
+// The service worker is a web API that helps you cache your assets and other files so that when the user is offline or on slow network, he/she can still see results on the screen, as such, it helps you build a better user experience, that's what you should know about service worker's for now. It's all about adding offline capabilities to your site
 registerServiceWorker();
